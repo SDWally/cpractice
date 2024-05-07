9008e0cb265446d1696745a081a45682658514fd
SDWally <1161512420@qq.com>
2024-05-08 01:47:02 +0800

    add computer room

---
+++ computer_room_system.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int select = 0;
+    cout << "welcome to the computer roon reservation system!!!" << endl;
+    cout << "please enter your identity." << endl;
+    cout << "\t\t ---------------------------\n";
+    cout << "\t\t |                          |\n";
+    cout << "\t\t |      1. 学生代表          |\n";
+    cout << "\t\t |                          |\n";
+    cout << "\t\t |      2. 老   师           |\n";
+    cout << "\t\t |                          |\n";
+    cout << "\t\t |      3. 管理员            |\n";
+    cout << "\t\t |                          |\n";
+    cout << "\t\t |      0. 退   出          |\n";
+    cout << "\t\t ---------------------------\n";
+    cout << "please enter your choice." << endl;
+    system("pause");
+    return 0;
+}
