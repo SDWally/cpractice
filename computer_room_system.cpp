3f62dad284b938ae64395ae7bd533c7f6523d7b6
SDWally <1161512420@qq.com>
2024-05-08 01:56:48 +0800

    add exit func

--- computer_room_system.cpp
+++ computer_room_system.cpp
@@ -5,19 +5,43 @@ using namespace std;
 int main()
 {
     int select = 0;
-    cout << "welcome to the computer roon reservation system!!!" << endl;
-    cout << "please enter your identity." << endl;
-    cout << "\t\t ---------------------------\n";
-    cout << "\t\t |                          |\n";
-    cout << "\t\t |      1. 学生代表          |\n";
-    cout << "\t\t |                          |\n";
-    cout << "\t\t |      2. 老   师           |\n";
-    cout << "\t\t |                          |\n";
-    cout << "\t\t |      3. 管理员            |\n";
-    cout << "\t\t |                          |\n";
-    cout << "\t\t |      0. 退   出          |\n";
-    cout << "\t\t ---------------------------\n";
-    cout << "please enter your choice." << endl;
+    while (true) 
+    {
+        cout << "welcome to the computer roon reservation system!!!" << endl;
+        cout << "please enter your identity." << endl;
+        cout << "\t\t ---------------------------\n";
+        cout << "\t\t |                          |\n";
+        cout << "\t\t |      1. 学生代表          |\n";
+        cout << "\t\t |                          |\n";
+        cout << "\t\t |      2. 老   师           |\n";
+        cout << "\t\t |                          |\n";
+        cout << "\t\t |      3. 管理员            |\n";
+        cout << "\t\t |                          |\n";
+        cout << "\t\t |      0. 退   出          |\n";
+        cout << "\t\t ---------------------------\n";
+        cout << "please enter your choice." << endl;   
+        cin >> select;
+        switch (select)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 0:
+                cout << "welcome to use next time." << endl;
+                system("pause");
+                return 0;
+                break;
+            default:
+                cout << "enter error, please choose again. " << endl;
+                system("pause");
+                system("cls");
+                break;
+        }
+    }
+
     system("pause");
     return 0;
 }
