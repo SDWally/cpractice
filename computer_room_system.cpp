ac462a8bfbd9e749e20d10c629eeb47e762b2dc3
SDWally <1161512420@qq.com>
2024-05-08 22:26:51 +0800

    add login func

--- computer_room_system.cpp
+++ computer_room_system.cpp
@@ -1,5 +1,62 @@
+#include "globalFile.hxx"
+#include "identity.hxx"
+#include <fstream>
+#include <string>
 #include <iostream>
 using namespace std;
+
+
+void LoginIn(string fileName, int type)
+{
+    Identity *penson = NULL;
+
+    ifstream ifs;
+    ifs.open(fileName, ios::in);
+
+    if (!ifs.is_open())
+    {
+        cout << "file is not exist." << endl;
+        ifs.close();
+        return;
+    }
+    int id = 0;
+    string name;
+    string pwd;
+    if (type == 1)
+    {
+        cout << "input your student id" << endl;
+        cin >> id;
+    }
+    else if (type == 2)
+    {
+        cout << "input your teacher id" << endl;
+        cin >> id;
+    }
+
+    cout << "input the username: " << endl;
+    cin >> name;
+
+    cout << "input the password:" << endl;
+    cin >> pwd;
+
+    if (type == 1)
+    {
+
+    }
+    else if (type == 2)
+    {
+
+    }
+    else if (type == 3)
+    {
+
+    }
+    cout << "auth failed!" << endl;
+    system("pause");
+    system("cls");
+    return;
+}
+
 
 
 int main()
@@ -23,22 +80,25 @@ int main()
         cin >> select;
         switch (select)
         {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 0:
-                cout << "welcome to use next time." << endl;
-                system("pause");
-                return 0;
-                break;
-            default:
-                cout << "enter error, please choose again. " << endl;
-                system("pause");
-                system("cls");
-                break;
+        case 1:
+            LoginIn(STUDENT_FILE, 1);
+            break;
+        case 2:
+            LoginIn(TEACHER_FILE, 1);
+            break;
+        case 3:
+            LoginIn(ADMIN_FILE, 1);
+            break;
+        case 0:
+            cout << "welcome to use next time." << endl;
+            system("pause");
+            return 0;
+            break;
+        default:
+            cout << "enter error, please choose again. " << endl;
+            system("pause");
+            system("cls");
+            break;
         }
     }
 
