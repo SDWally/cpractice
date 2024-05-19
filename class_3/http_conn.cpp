44c66d61d65d282c1c32ae94aa65941500821175
SDWally <1161512420@qq.com>
2024-05-19 22:57:41 +0800

    add http cpp

---
+++ class_3/http_conn.cpp
@@ -0,0 +1,150 @@
+#pragma once
+#include "http_conn.h"
+#include "log.h"
+#include <map>
+#include <mysql/mysql.h>
+#include <fstream>
+
+#define connfdLT
+#define listenfdLT
+
+const char *ok_200_title = "OK";
+const char *error_400_title = "Bad Request";
+const char *error_400_form = "Your request has bad syntax or is inherently impossible to staisfy.\n";
+const char *error_403_title = "Forbidden";
+const char *error_403_form = "You do not have permission to get file from this server.\n";
+const char *error_404_title = "Not Found";
+const char *error_404_form = "The requested file was not found on this server.\n";
+const char *error_500_title = "Interval Error";
+const char *error_500_form = "There was an unusual problem serving the request file.\n";
+
+const char *doc_root = "/home/class3/root/";
+
+map<string, string> users;
+locker m_lock;
+
+void http_conn::initmysql_result(connection_pool *connPool)
+{
+    MYSQL *mysql = NULL;
+    connectionRAII mysqlcon(&mysql, connPool);
+    if (mysql_query(mysql, "select username, passwd from user")
+    {
+        LOG_ERROR("select error: %s\n", mysql_error(mysql));
+    })
+    MYSQL_RES *result = mysql_store_result(mysql);
+    int num_fields = mysql_num_fields(result);
+    MYSQL_FIELD *fields = mysql_fetch_fields(result);
+    while (MYSQL_ROW row = mysql_fetch_row(result))
+    {
+        string temp1(row[0]);
+        string temp2(row[1]);
+        users[temp1] = temp2;
+    }
+}
+
+int setnonblocking(int fd)
+{
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+void addfd(int epollfd, int fd, bool one_shot)
+{
+    epoll_event event;
+    event.data.fd = fd;
+
+
+#ifdef connfdET
+    event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+#endif
+
+#ifdef conffdLT
+    event.events = EPOLLIN | EPOLLRDHUP;
+#endif
+
+#ifdef listenfdET
+    event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+#endif
+
+#ifdef listenfdLT
+    event.events = EPOLLIN | EPOLLRDHUP;
+#endif
+    if (one_shot)
+    {
+        event.events |= EPOLLONESHOT;
+    }
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+void removefd(int epollfd, int fd)
+{
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+    close(fd);
+}
+
+void modfd(int epollfd, int fd, int ev)
+{
+    epoll_event event;
+    event.data.fd = fd;
+
+#ifdef connfdET
+    event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+#endif
+
+#ifdef connfdLT
+    event.events = ev | EPOLLONESHOT | EPOLLRDHUP;
+#endif
+    
+    epoll_ctl(epollfd, EPOLL_STL_MOD, fd, &event);
+}
+
+
+int http_conn::m_user_count = 0;
+int http_conn::m_epollfd = -1;
+
+void http_conn::close_conn(bool real_close)
+{
+    if (real_close && (m_sockfd != -1))
+    {
+        removefd(m_epollfd, m_sockfd);
+        m_sockfd = -1;
+        m_user_count--;
+    }
+}
+
+void http_conn::init(int sockfd, const sockaddr_in &addr)
+{
+    m_sockfd = sockdf;
+    m_address = addr;
+    addfd(m_epollfd, sockfd, true);
+    m_user_count++;
+    init();
+}
+
+
+void http_conn::init()
+{
+    mysql = NULL;
+    bytes_to_send = 0;
+    bytes_have_send = 0;
+    m_check_state = CHECK_STATE_REQUESTLINE;
+    m_linger = false;
+    m_method = GET;
+    m_url = 0;
+    m_version = 0;
+    m_content_length = 0;
+    m_host = 0;
+    m_start_line = 0;
+    m_checked_idx = 0;
+    m_read_idx = 0;
+    m_write_idx = 0;
+    cgi = 0;
+    memset(m_read_buf, '\0', READ_BUFFER_SIZE);
+    memset(m_write_buf, '\0', WRITE_BUFFER_SIZE);
+    memset(m_real_file, '\0', FILENAME_LEN);
+}
+
+
