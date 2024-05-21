1966e67d64fb58c093f4d8868a2234f25f81e705
SDWally <1161512420@qq.com>
2024-05-22 00:58:50 +0800

    update main

--- class_3/main.c
+++ class_3/main.c
@@ -11,3 +11,191 @@
 
 #include "locker.h"
 #include "threadpool.h"
+#include "lst_timer.h"
+#include "http_conn.h"
+#include "log.h"
+#include "sql_connection_pool.h"
+
+#define MAX_FD 65536
+#define MAX_EVENT_NUMBER 10000
+#define TIMESLOT 5
+
+#define SYNLOG
+
+#define listenfdLT
+
+extern int addfd(int epollfd, int fd, bool one_shot);
+extern int remove(int epollfd, int fd);
+extern int setnonblocking(int fd);
+
+static int pipefd[2];
+static sort_timer_lst timer_lst;
+static int epollfd = 0;
+
+
+void sig_handler(int sig)
+{
+    int save_errno = errno;
+    int msg = sig;
+    send(pipefd[1], (char *) &msg, 1, 0)
+    errno = save_errno;
+}
+
+void addsig(int sig, void(handler)(int), bool restart = true)
+{
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = handler;
+    if (restart)
+    {
+        sa.sa_flags |= SA_RESTART;
+    }
+
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+void timer_handler()
+{
+    timer_lst.tick();
+    alarm(TIMESLOT);
+}
+
+void cb_func(client_data *user_data)
+{
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0);
+    assert(user_data);
+    close(user_data->sockfd);
+    http_conn::m_user_count--;
+    LOG_INFO("close fd %d", user_data->sockfd);
+    Log::get_instance()->flush();
+}
+
+void show_error(int connfd, const char *info)
+{
+    printf("%s", info);
+    send(connfd, info, strlen(info), 0);
+    close(connfd);
+}
+
+int main(int argc, char *argv[])
+{
+#ifdef ASYNLOG
+    Log::get_instance()->init("ServerLog", 2000, 800000, 8);
+#endif
+#ifdef SYNLOG
+    Log::get_instance()->init("ServerLog", 2000, 800000, 8);
+#endif
+
+    if (argc <= 1)
+    {
+        printf("usage: %s ip_address port number\n"), basename(argv[0]);
+        return 1;
+    }
+    int port = atol(argv[1]);
+    addsig(SIGPIPE, SIG_IGN);
+
+    connection_pool *connPool = connection_pool::GetInstance();
+    connPool->init("localhost", "root", "root", "qgydb", 3306, 8);
+    
+    threadpool<http_conn> *pool = NULL;
+    try
+    {
+        pool = new threadpool<http_conn>(connPool);
+    }
+    catch (...)
+    {
+        return 1;
+    }
+
+    http_conn *users = new http_conn[MAX_FD];
+    assert(users);
+    users->initmysql_result(connPool);
+    int listenfd = socker(PF_INET, SOCK_STREM, 0);
+    assert(listenfd >= 0);
+
+    int ret = 0;
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sim_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htonl(port);
+
+    int flag = 1;
+    setsocket(listenfd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
+    ret = bind(listenfd, (struct sockaddr *)&address, sizeof(address));
+    assert(ret >= 0);
+    ret = listen(listenfd, 5);
+    assert(ret >= 0);
+
+    epoll_event events[MAX_EVENT_NUMBER];
+    epollfd = epoll_create(5);
+    assert(epollfd != -1);
+
+    addfd(epollfd, listenfd, false);
+    http_conn::m_epollfd = epollfd;
+
+    ret = socketpair(PF_UNIX, SOCK_STREAM, 000, pipefd);
+    assert(ret != -1);
+    setnonblocking(pipefd[1]);
+    addfd(epollfd, pipefd[0], false);
+
+    addsig(SIGALRM, sig_handler, false);
+    addsig(SIGTERM, sig_handler, false);
+    bool stop_server = false;
+
+    client_data *users_timer = new client_data[MAX_FD];
+
+    bool timeout = false;
+    alarm(TIMESLOT);
+
+    while (!stop_server)
+    {
+        int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if (number < 0 && errno != EINTR)
+        {
+            LOG_ERROR("%s", "epoll failure");
+            break;
+        }
+
+        for (int i = 0; i < number; i++)
+        {
+            int sockfd = events[i].data.fd;
+            if (sockfd == listenfd)
+            {
+                struct sockaddr_in client_address;
+                socklen_t client_addrlength = sizeof(client_address);
+#ifdef listenfdLT
+                int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlength);
+                if (connfd < 0)
+                {
+                    LOG_ERROR("%s:errno is: %d", "accept error", errno);
+                    continue;
+                }
+                if (http_conn::m_user_count >= MAX_FD)
+                {
+                    show_error(connfd, "Internal server busy");
+                    LOG_ERROR("%s", "Internal server busy");
+                    continue;
+                }               
+                users[connfd].init(connfd, client_address);
+                users_timer[connfd].address = client_address;
+                users_timer[connfd].sockfd = connfd;
+                util_timer *timer = new util_timer;
+                timer->user_data = &users_timer[connfd];
+                timer->cb_func = cb_func;
+                time_t cur = time(NULL);
+                timer->expire = cur + 3 * TIMESLOT;
+                users_timer[connfd].timer = timer;
+                timer_lst.add_timer(timer);
+#endif
+
+
+
+            }
+        }
+    }
+
+
+
+}
